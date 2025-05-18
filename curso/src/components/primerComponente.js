fcd41e97467797f728ca5ed50f0fd9c2dcd94dc5
JFChavezC001 <jfchavezc001@gmail.com>
2025-05-18 14:07:24 -0600

    Agregamos un array dentro de la aplicacion y lo mostramos a traves de una lista

--- curso/src/components/primerComponente.js
+++ curso/src/components/primerComponente.js
@@ -4,6 +4,13 @@ export const primerComponente = () => {
     //creamos una variable 
     let nombre = "José Francisco Chávez Castillo";
     let numCuenta= 1822407;
+    //creamos un array
+
+    let cursos =[
+      "Programacion",
+      "Análisis de datos",
+      "Geoestadistica"
+    ]
     return (
     
     
@@ -11,6 +18,18 @@ export const primerComponente = () => {
         <h1>Hola</h1>
         <h1>estoy creando mi primer componente, mi nombre es {nombre}</h1>{/*para mostrar una variable se hace uso de llaves {} y dentro colocamos nuestra variable*/ }
         <p>Este es mi primer texto, mi cuenta es {numCuenta}</p>
+
+        <ul>
+          {
+            cursos.map(cursos =>{
+              return(<li>
+                {cursos}
+              </li>)
+            }
+
+            )
+          }
+        </ul>
     </div>
     
   );
